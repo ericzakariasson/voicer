4ab481adbac4f4a4c53db3f7f5ab95bc9c434176
ericzakariasson <eric.zakariasson@gmail.com>
2023-04-11 15:17:51 +0200

    feat: add app

---
+++ voicer.py
@@ -0,0 +1,100 @@
+import rumps
+import pyaudio
+import wave
+import threading
+import time
+import os
+import openai
+from pynput.keyboard import Controller, Key
+
+# Recording settings
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+# OpenAI settings
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Global variables for recording
+audio = pyaudio.PyAudio()
+stream = None
+frames = []
+is_recording = False
+
+# Pynput keyboard controller
+
+keyboard = Controller()
+
+def type_text(text):
+    for char in text:
+        if char == '\n':
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+        else:
+            keyboard.type(char)
+        time.sleep(0.01)
+
+
+def start_recording():
+    print("Recording...")
+    global stream, frames
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True, frames_per_buffer=CHUNK)
+    frames = []
+
+    while is_recording:
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+
+def save_audio_file(filename='output.wav'):
+    print("Saving audio file...")
+    global stream, frames
+
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    stream.stop_stream()
+    stream.close()
+
+
+def convert_audio_to_text(filename='output.wav'):
+    print("Converting audio to text...")
+    # return "Hello World"
+    with open(filename, 'rb') as audio_file:
+        response = openai.Audio.transcribe("whisper-1", audio_file)
+
+    print("Response: ")
+    print(response)
+    return response.text
+
+
+class Voicer(rumps.App):
+    def __init__(self):
+        super(Voicer, self).__init__("Voicer")
+        self.menu = [rumps.MenuItem(
+            "Start Recording", callback=self.toggle_recording)]
+
+    def toggle_recording(self, sender):
+        global is_recording
+
+        if not is_recording:
+            is_recording = True
+            sender.title = 'Stop Recording'
+            threading.Thread(target=start_recording).start()
+        else:
+            is_recording = False
+            sender.title = 'Start Recording'
+            save_audio_file()
+            transcription = convert_audio_to_text()
+            if transcription:
+                type_text(transcription)
+
+
+if __name__ == "__main__":
+    Voicer().run()
